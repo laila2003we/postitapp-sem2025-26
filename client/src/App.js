c0f30afd208264c985933122886a05a9a2c49631
Web Development II <lailaalshahri5@gmail.com>
2025-09-22 13:36:01 +0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import About from "./Components/About";
+
+const App = () => {
+  return (
+    <>
+      <Login />
+      <Home />
+      <About />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/Components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>©2025.PostIT.Laila 2021393058. All Rights Reserved.</div>
+    </footer>
+  );
+};
+
+export default Footer;
